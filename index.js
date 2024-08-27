f0aad463c97e20d7228780cf63292f04dffbe69a
Abdulmajeed Alselmi <hh738hhh@gmail.com>
2024-08-28 02:38:00 +0300

    solved

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const number = new Set();
+
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    if (number.has(complement)) {
+      return true;
+    }
+    number.add(array[i]);
+  }
+  return false;
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
+*/
+/* 
+  Time Complexity: O(n)
+  Space Complexity: O(n)
 */
 
 /* 
@@ -13,6 +28,7 @@ function hasTargetSum(array, target) {
 /*
   Add written explanation of your solution here
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
